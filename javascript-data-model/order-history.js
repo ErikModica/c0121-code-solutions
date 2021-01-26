aa18393de690e5c635000a7d3c9eac02c1d83b6f
Erik Modica <erikmodica@gmail.com>
2021-01-26 14:48:24 -0800

    revised javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,142 +1,82 @@
 var orderHistory = [
   {
-    orderPlacedDate: {
-      day: 4,
-      month: 8,
-      year: 2020
-    },
-    displayImage: 6305834,
+    orderPlacedDate: 20200804,
+    shipTo: 'JS Masher',
     orderID: '14439416898772232',
-    name: 'JS Masher',
-    author: {
-      firstName: 'Rauschmayer',
-      lastName: 'Axel',
-      title: 'Dr.'
-    },
-    reviews: [],
-    title: 'JavaScript for impatient programmers',
-    finalReturnDate: {
-      day: 7,
-      month: 9,
-      year: 2020
-    },
-    dateDelivered: {
-      day: 8,
-      month: 8,
-      year: 2020
-    },
-    costOfItem: 31.55,
-    costOfShipping: 2.45
+    items:
+    [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: {
+          firstName: 'Rauschmayer',
+          lastName: 'Axel',
+          title: 'Dr.'
+        },
+        costOfItem: 31.55,
+        reviews: [],
+        dateDelivered: 20200808,
+        finalReturnDate: 20200907
+      }
+    ]
   },
 
   {
-    orderPlacedDate: {
-      day: 19,
-      month: 7,
-      year: 2020
-    },
-    displayImage: 907852,
+    orderPlacedDate: 20200719,
+    shipTo: 'JS Masher',
     orderID: '11399842681290257',
-    name: 'JS Masher',
-    author: {
-      firstName: 'Alexander',
-      lastName: 'Christopher',
-      title: ''
-    },
-    reviews: [],
-    title: 'The Timeless Way of Building',
-    finalReturnDate: {
-      day: 19,
-      month: 8,
-      year: 2020
-    },
-    dateDelivered: {
-      day: 20,
-      month: 7,
-      year: 2020
-    },
-    costOfItem: 41.33,
-    costOfShipping: 3.20
+    items:
+    [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: {
+          firstName: 'Alexander',
+          lastName: 'Christopher'
+        },
+        costOfItem: 41.33,
+        reviews: [],
+        dateDelivered: 20200720,
+        finalReturnDate: 20200819
+      }
+    ]
   },
 
   {
-    orderPlacedDate: {
-      day: 4,
-      month: 7,
-      year: 2020
-    },
-    displayImage: 5627324,
+    orderPlacedDate: 20200707,
+    shipTo: 'JS Masher',
     orderID: '11428755579059409',
-    name: 'JS Masher',
-    reviews: [],
-    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapeter super long title that is too long ;(',
-    finalReturnDate: {
-      day: 5,
-      month: 8,
-      year: 2020
-    },
-    dateDelivered: {
-      day: 7,
-      month: 7,
-      year: 2020
-    },
-    costOfItem: 15.98,
-    costOfShipping: 1.24
+    items:
+    [
+      {
+        title: 'Gamecub Controller Adapeter. Super Smash Bros Switch Gamecub Adapter for WII U, PC. Support Turbo and Vibration Features.',
+        costOfItem: 15.98,
+        reviews: [],
+        dateDelivered: 20200720,
+        finalReturnDate: 20200819
+      }
+    ]
   },
 
   {
-    orderPlacedDate: {
-      day: 3,
-      month: 7,
-      year: 2020
-    },
-    displayImage: 7237724,
+    orderPlacedDate: 20200703,
+    shipTo: 'JS Masher',
     orderID: '11328831772648248',
-    name: 'JS Masher',
-    reviews: [],
-    title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    finalReturnDate: {
-      day: 4,
-      month: 8,
-      year: 2020
-    },
-    dateDelivered: {
-      day: 5,
-      month: 7,
-      year: 2020
-    },
-    costOfItem: 94.95,
-    costOfShipping: 0
-  },
-
-  {
-    orderPlacedDate: {
-      day: 3,
-      month: 7,
-      year: 2020
-    },
-    displayImage: 3486579,
-    orderID: '11328831772648248',
-    name: 'JS Masher',
-    author: {
-      firstName: 'Stephane',
-      lastName: 'Faroult',
-      title: ''
-    },
-    reviews: [],
-    title: 'The Art of Sql',
-    finalReturnDate: {
-      day: 4,
-      month: 8,
-      year: 2020
-    },
-    dateDelivered: {
-      day: 5,
-      month: 7,
-      year: 2020
-    },
-    costOfItem: 33.99,
-    costOfShipping: 10.00
+    items:
+    [
+      {
+        title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        costOfItem: 94.95,
+        reviews: [],
+        dateDelivered: 20200705,
+        finalReturnDate: 20200804
+      },
+      {
+        title: 'The Art of Sql',
+        costOfItem: 33.99,
+        reviews: [],
+        dateDelivered: 20200705,
+        finalReturnDate: 20200804
+      }
+    ]
   }
 ];
 
