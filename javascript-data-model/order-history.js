29b78f6423d1cf9cf26ccbb027c38400c7f21b91
Erik Modica <erikmodica@gmail.com>
2021-01-26 11:38:08 -0800

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,238 @@
+var orderHistory = [
+  {
+    orderPlacedDate: {
+      day: 4,
+      month: 8,
+      year: 2020
+    },
+    displayImage: 6305834,
+    orderID: '14439416898772232',
+    shipTo: {
+      name: 'JS Masher',
+      address: '12 Windylane',
+      suite: '',
+      city: 'Whoville',
+      zipcode: '90394',
+      state: 'PA',
+      email: 'iamsupercool@gmail.com'
+    },
+    orderDetails: {
+      manufacturer: 'Cool Kid Company',
+      seller: 'George',
+      weight: 5,
+      author: {
+        firstName: 'Rauschmayer',
+        lastName: 'Axel',
+        title: 'Dr.'
+      },
+      otherInfo: ''
+    },
+    invoice: {
+      info: '',
+      info2: '',
+      info3: ''
+    },
+    reviews: ['super dope book!', 'I LOVE JAVASCRIPT NOW', 'greatest read of my life but the package arrived a day later than it was supposed to, 2 stars'],
+    title: 'JavaScript for impatient programmers',
+    finalReturnDate: {
+      day: 7,
+      month: 9,
+      year: 2020
+    },
+    dateDelivered: {
+      day: 8,
+      month: 8,
+      year: 2020
+    },
+    costOfItem: 31.55,
+    costOfShipping: 2.45
+  },
+
+  {
+    orderPlacedDate: {
+      day: 19,
+      month: 7,
+      year: 2020
+    },
+    displayImage: 907852,
+    orderID: '11399842681290257',
+    shipTo: {
+      name: 'JS Masher',
+      address: '12 Windylane',
+      suite: '',
+      city: 'Whoville',
+      zipcode: '90394',
+      state: 'PA',
+      email: 'iamsupercool@gmail.com'
+    },
+    orderDetails: {
+      manufacturer: 'We Love Making Books Corp',
+      seller: 'Gavin',
+      weight: 6.7,
+      author: {
+        firstName: 'Alexander',
+        lastName: 'Christopher',
+        title: ''
+      },
+      otherInfo: ''
+    },
+    invoice: {
+      info: '',
+      info2: '',
+      info3: ''
+    },
+    reviews: ['I loved it!', 'a truly timeless way of building it was', 'life changing'],
+    title: 'The Timeless Way of Building',
+    finalReturnDate: {
+      day: 19,
+      month: 8,
+      year: 2020
+    },
+    dateDelivered: {
+      day: 20,
+      month: 7,
+      year: 2020
+    },
+    costOfItem: 41.33,
+    costOfShipping: 3.20
+  },
+
+  {
+    orderPlacedDate: {
+      day: 4,
+      month: 7,
+      year: 2020
+    },
+    displayImage: 90737724,
+    orderID: '11428755579059409',
+    shipTo: {
+      name: 'JS Masher',
+      address: '12 Windylane',
+      suite: '',
+      city: 'Whoville',
+      zipcode: '90394',
+      state: 'PA',
+      email: 'iamsupercool@gmail.com'
+    },
+    orderDetails: {
+      manufacturer: 'Tech Genius',
+      seller: 'Gerald',
+      weight: 2,
+      otherInfo: ''
+    },
+    invoice: {
+      info: '',
+      info2: '',
+      info3: ''
+    },
+    reviews: ['now i can play Billy Hatcher and the Giant Egg!', 'super duper fun stuff', 'GAMECUBE!!!!!!!!'],
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapeter super long title that is too long ;(',
+    finalReturnDate: {
+      day: 5,
+      month: 8,
+      year: 2020
+    },
+    dateDelivered: {
+      day: 7,
+      month: 7,
+      year: 2020
+    },
+    costOfItem: 15.98,
+    costOfShipping: 1.24
+  },
+
+  {
+    orderPlacedDate: {
+      day: 3,
+      month: 7,
+      year: 2020
+    },
+    displayImage: 7237724,
+    orderID: '11328831772648248',
+    shipTo: {
+      name: 'JS Masher',
+      address: '12 Windylane',
+      suite: '',
+      city: 'Whoville',
+      zipcode: '90394',
+      state: 'PA',
+      email: 'iamsupercool@gmail.com'
+    },
+    orderDetails: {
+      manufacturer: 'Tech Genius',
+      seller: 'Giovani',
+      weight: 8.3,
+      otherInfo: ''
+    },
+    invoice: {
+      info: '',
+      info2: '',
+      info3: ''
+    },
+    reviews: ['gamers love this item', 'favorite controller!', 'GAMECUBE!!!!!!!!'],
+    title: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    finalReturnDate: {
+      day: 4,
+      month: 8,
+      year: 2020
+    },
+    dateDelivered: {
+      day: 5,
+      month: 7,
+      year: 2020
+    },
+    costOfItem: 94.95,
+    costOfShipping: 0
+  },
+
+  {
+    orderPlacedDate: {
+      day: 3,
+      month: 7,
+      year: 2020
+    },
+    displayImage: 3486579,
+    orderID: '11328831772648248',
+    shipTo: {
+      name: 'JS Masher',
+      address: '12 Windylane',
+      suite: '',
+      city: 'Whoville',
+      zipcode: '90394',
+      state: 'PA',
+      email: 'iamsupercool@gmail.com'
+    },
+    orderDetails: {
+      manufacturer: 'Bookmen',
+      seller: 'Gregory',
+      weight: 4.87,
+      author: {
+        firstName: 'Stephane',
+        lastName: 'Faroult',
+        title: ''
+      },
+      otherInfo: ''
+    },
+    invoice: {
+      info: '',
+      info2: '',
+      info3: ''
+    },
+    reviews: ['sql is made interesting with this book', 'tough but good read', 'learned alot!', 'cannot recommend this enough'],
+    title: 'The Art of Sql',
+    finalReturnDate: {
+      day: 4,
+      month: 8,
+      year: 2020
+    },
+    dateDelivered: {
+      day: 5,
+      month: 7,
+      year: 2020
+    },
+    costOfItem: 33.99,
+    costOfShipping: 10.00
+  }
+];
+
+console.log('oderHistory Array: ', orderHistory);
