5d8652d5e581401213f5d41cd192dc27f5688bce
Erik Modica <erikmodica@gmail.com>
2021-02-02 11:33:00 -0800

    revision

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -5,12 +5,12 @@ var switchOff = true;
 $bulbButton.addEventListener('click', function (event) {
 
   if (switchOff) {
-    $bulbButton.className = 'bulb ' + 'bulb-light';
-    $backgroundColor.className = 'background ' + 'background-light';
+    $bulbButton.className = 'bulb bulb-light';
+    $backgroundColor.className = 'background background-light';
     switchOff = false;
   } else if (!switchOff) {
-    $bulbButton.className = 'bulb ' + 'bulb-dark';
-    $backgroundColor.className = 'background ' + 'background-dark';
+    $bulbButton.className = 'bulb bulb-dark';
+    $backgroundColor.className = 'background background-dark';
     switchOff = true;
   }
 });
