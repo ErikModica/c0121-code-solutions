f60ead24340ebbe58a44659409db5d0dac86bdbc
Erik Modica <erikmodica@gmail.com>
2021-02-03 17:06:20 -0800

    revision

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,23 @@
 var $typedLetter = document.querySelector('body');
 var $spanText = document.querySelectorAll('span');
 var i = 0;
+var ifSpace = false;
 
-$spanText[i].className = 'current-letter';
+$spanText[0].className = 'current-letter';
 
 $typedLetter.addEventListener('keydown', function (event) {
 
   // console.log(event.key)
   // console.log($spanText[i].textContent)
   // console.log(i);
+  // console.log(event.code);
+  // console.log($spanText[i].className)
+
+  if ($spanText[i].className === 'space current-letter') {
+    ifSpace = true;
+  }
+
+  // console.log(ifSpace);
 
   if (event.key === $spanText[i].textContent) {
     $spanText[i].className = ' right-letter';
@@ -16,12 +25,18 @@ $typedLetter.addEventListener('keydown', function (event) {
       $spanText[i].className += ' space';
     }
     i++;
-  } else { $spanText[i].className = ' wrong-letter'; }
+  } else { $spanText[i].className = 'wrong-letter'; }
   // if (event.code === 'Space') {
   //   $spanText[i].className = 'wrong-letter space';
   // }
 
-  $spanText[i].className = 'current-letter';
+  $spanText[i].className += ' current-letter';
+
+  if (ifSpace) {
+    $spanText[i].className += ' space';
+  }
+
+  ifSpace = false;
 
 });
 
