96a66e6aa8b22ec06653df60e8a8613d9622f429
Erik Modica <erikmodica@gmail.com>
2021-02-05 15:19:16 -0800

    string-manipulation-2 completed

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,24 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+
+  var newString = '';
+  var firstChar = '';
+  var secondChar = '';
+
+  firstChar = string[firstIndex];
+  secondChar = string[secondIndex];
+
+  for (var i = 0; i < string.length; i++) {
+
+    if (i === firstIndex) {
+      newString += secondChar;
+    } else if (i === secondIndex) {
+      newString += firstChar;
+    } else {
+      newString += string[i];
+    }
+
+  }
+  return newString;
+}
