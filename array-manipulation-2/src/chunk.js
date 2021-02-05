55ca3743dd3e6f1b3c831d83899252a647f99c53
Erik Modica <erikmodica@gmail.com>
2021-02-04 18:38:09 -0800

    array-manipulation-2 completed

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,24 +1,18 @@
 /* exported chunk */
 
 function chunk(array, size) {
-  var returnArray = [];
-  var sizeCount = 0;
-  var sizeArray = [];
 
-  for (var i = 0; i <= array.length; i++) {
+  var returnAr = [];
+  var sizeAr = [];
 
-    if (sizeCount !== size) {
-      sizeArray.push(array[i]);
-    } else {
-      returnArray.push(sizeArray);
-      sizeArray = [];
+  for (var i = 0; i < array.length; i++) {
 
-      sizeArray.push(array[i]);
-      if (array[i] !== undefined) {
-        sizeCount = 0;
-      }
+    sizeAr.push(array[i]);
+
+    if (sizeAr.length === size || i === array.length - 1) {
+      returnAr.push(sizeAr);
+      sizeAr = [];
     }
-    sizeCount++;
   }
-  return returnArray;
+  return returnAr;
 }
