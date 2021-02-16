f57c4911a1b3fea032ea5e0a24bb4eaac4f00ce7
Erik Modica <erikmodica@gmail.com>
2021-02-15 18:06:24 -0800

    cleaned up ugly code and made it a bit prettier

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -4,13 +4,12 @@ var $container = document.querySelector('.container');
 var $noButton = document.querySelector('.no-button');
 
 $openButton.addEventListener('click', function (event) {
-  $modalDialog.className = 'modal-dialog dialog-active';
-  $container.className = 'container container-active';
-  $openButton.className = 'open-button open-active';
-});
-
-$noButton.addEventListener('click', function (event) {
   $modalDialog.className = 'modal-dialog';
   $container.className = 'container';
   $openButton.className = 'open-button';
+});
+
+$noButton.addEventListener('click', function (event) {
+  $modalDialog.className = 'modal-dialog hidden';
+  $container.className = 'container hidden';
 });
