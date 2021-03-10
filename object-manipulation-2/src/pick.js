08a37390fd46dae3d642f2235da5137f4561351a
ErikModica <erikmodica@gmail.com>
2021-03-09 18:28:31 -0800

    attempt at revisions

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,55 +1,51 @@
 /* exported pick */
 
+// function pick(source, keys) {
+//   var newObject = {};
+//   var newIndex = 0;
+//   var testArray = [];
+//   var newKey;
+
+//   for (var testKey in source) {
+//     testArray.push(testKey);
+//   }
+
+//   if (testArray.length < keys.length) {
+//     var arrayDiff = keys.length - testArray.length;
+
+//     for (var i = 0; i < arrayDiff; i++) {
+//       newKey = 'newKey' + i;
+//       source[newKey] = 'forbiddenValue';
+//     }
+
+//     for (var key in source) {
+//       if (key === testArray[newIndex] && source[key] !== undefined) {
+//         newObject[key] = source[key];
+//         newIndex++;
+//       }
+//     }
+//   }
+
+//   for (var regKey in source) {
+//     if (regKey === keys[newIndex] && source[regKey] !== undefined) {
+//       newObject[regKey] = source[regKey];
+//       newIndex++;
+//     }
+//   }
+
+//   return newObject;
+// }
+
 function pick(source, keys) {
-  var newObject = {};
-  var newIndex = 0;
-  var testArray = [];
-  var newKey;
+  const pickedObj = {};
 
-  for (var testKey in source) {
-    testArray.push(testKey);
-  }
-
-  if (testArray.length < keys.length) {
-    var arrayDiff = keys.length - testArray.length;
-
-    for (var i = 0; i < arrayDiff; i++) {
-      newKey = 'newKey' + i;
-      source[newKey] = 'forbiddenValue';
-    }
-
-    for (var key in source) {
-      if (key === testArray[newIndex] && source[key] !== undefined) {
-        newObject[key] = source[key];
-        newIndex++;
-      }
+  for (let i = 0; i < keys.length; i++) {
+    for (keys[i] in source) {
+      pickedObj[keys[i]] = source[keys[i]];
     }
   }
-
-  for (var regKey in source) {
-    if (regKey === keys[newIndex] && source[regKey] !== undefined) {
-      newObject[regKey] = source[regKey];
-      newIndex++;
-    }
-  }
-
-  return newObject;
+  console.log(pickedObj);
 }
 
-//   for (var key in source) {
-
-//     if (key === keys[i]) {
-
-//       console.dir(source);
-
-//       console.log(key);
-//       console.log(keys[i]);
-
-//       newObject[key] = source[key];
-//       i++
-//     }
-
-//     }
-//     return newObject;
-
-// }
+const testObj = { firstName: 'Evil', lastName: 'Ewok' };
+const testArr = ['lastName'];
