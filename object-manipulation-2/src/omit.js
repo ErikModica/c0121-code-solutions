b32fd4872a15bb56c375dbf6d87eb4832fc688c5
ErikModica <erikmodica@gmail.com>
2021-03-11 16:46:08 -0800

    cutting down code

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,37 +1,18 @@
 /* exported omit */
 
 function omit(source, keys) {
-  var newObject = {};
-  var newIndex = 0;
-  var testArray = [];
-  var newKey;
-
-  for (var testKey in source) {
-    testArray.push(testKey);
-  }
-
-  if (testArray.length < keys.length) {
-    var arrayDiff = keys.length - testArray.length;
-
-    for (var i = 0; i < arrayDiff; i++) {
-      newKey = 'newKey' + i;
-      source[newKey] = 'forbiddenValue';
-    }
-
-    for (var key in source) {
-      if (key !== testArray[newIndex] && source[key] !== undefined && source[key] !== 'forbiddenValue') {
-        newObject[key] = source[key];
-        newIndex++;
+  const newObject = {};
+  let isAKey = false;
+  for (const key in source) {
+    for (let i = 0; i < keys.length; i++) {
+      if (key === keys[i]) {
+        isAKey = true;
       }
     }
-  }
-
-  for (var regKey in source) {
-    if (regKey !== keys[newIndex] && source[regKey] !== undefined && source[regKey] !== 'forbiddenValue') {
-      newObject[regKey] = source[regKey];
-      newIndex++;
+    if (!isAKey) {
+      newObject[key] = source[key];
     }
+    isAKey = false;
   }
-
   return newObject;
 }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,51 +1,13 @@
 /* exported pick */
 
-// function pick(source, keys) {
-//   var newObject = {};
-//   var newIndex = 0;
-//   var testArray = [];
-//   var newKey;
-
-//   for (var testKey in source) {
-//     testArray.push(testKey);
-//   }
-
-//   if (testArray.length < keys.length) {
-//     var arrayDiff = keys.length - testArray.length;
-
-//     for (var i = 0; i < arrayDiff; i++) {
-//       newKey = 'newKey' + i;
-//       source[newKey] = 'forbiddenValue';
-//     }
-
-//     for (var key in source) {
-//       if (key === testArray[newIndex] && source[key] !== undefined) {
-//         newObject[key] = source[key];
-//         newIndex++;
-//       }
-//     }
-//   }
-
-//   for (var regKey in source) {
-//     if (regKey === keys[newIndex] && source[regKey] !== undefined) {
-//       newObject[regKey] = source[regKey];
-//       newIndex++;
-//     }
-//   }
-
-//   return newObject;
-// }
-
 function pick(source, keys) {
-  const pickedObj = {};
-
-  for (let i = 0; i < keys.length; i++) {
-    for (keys[i] in source) {
-      pickedObj[keys[i]] = source[keys[i]];
+  const newObject = {};
+  for (const key in source) {
+    for (let i = 0; i < keys.length; i++) {
+      if (key === keys[i] && source[key] !== undefined) {
+        newObject[keys[i]] = source[key];
+      }
     }
   }
-  console.log(pickedObj);
+  return newObject;
 }
-
-const testObj = { firstName: 'Evil', lastName: 'Ewok' };
-const testArr = ['lastName'];
