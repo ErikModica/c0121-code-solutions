f622455dc0c2a85a21414964a6a2f4b9b3cf304a
ErikModica <erikmodica@gmail.com>
2021-03-24 12:54:34 -0700

    revision

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 const CustomButton = () => <button>Click Me!</button>;
 const $root = document.querySelector('#root');
 
-ReactDOM.render(CustomButton(), $root);
+ReactDOM.render(<CustomButton />, $root);
