24d84cec1d6de27767ae2bee06d930780354f724
ErikModica <erikmodica@gmail.com>
2021-03-24 18:34:21 -0700

    react-events-and-state completed

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    const isClicked = this.state.isClicked;
+    let button;
+    if (!isClicked) {
+      return <button onClick={this.handleClick}>Click Me!</button>;
+    } else {
+      return <button>Thanks!</button>;
+    }
+  }
+}
+
+ReactDOM.render(<CustomButton />, document.querySelector('#root'));
