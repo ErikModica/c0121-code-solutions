cb591e82585df497bb201223739114fe39bc06e2
ErikModica <erikmodica@gmail.com>
2021-03-26 16:29:18 -0700

    react-accordion-component completed

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -5,20 +5,27 @@ class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = { bodyVisible: false };
-    this.showBody = this.showBody.bind(this);
+    this.closeBody = this.closeBody.bind(this);
+    this.decipherID = this.decipherID.bind(this);
   }
 
-  showBody(event) {
-    this.setState({ bodyVisible: true });
-    // console.log(this.props);
-    // console.log(event.target);
+  decipherID(event) {
+    for (let i = 0; i < this.props.topics.length; i++) {
+      if (this.props.topics[i].heading === event.target.textContent) {
+        this.setState({ currentID: this.props.topics[i].id });
+      }
+    }
+  }
+
+  closeBody() {
+    this.setState({ currentID: 0 });
   }
 
   render() {
     const topicsList = this.props.topics.map(topic => (
       <div key={topic.id}>
-        <h1 key={topic.id} onClick={this.showBody} className="head">{topic.heading}</h1>
-        <p className="body hidden">{topic.body}</p>
+        <h1 onDoubleClick={this.closeBody} onClick={this.decipherID} className="head">{topic.heading}</h1>
+        <p onClick={this.closeBody} className={topic.id === this.state.currentID ? 'body' : 'body hidden'}>{topic.body}</p>
       </div>
     ));
     return topicsList;
