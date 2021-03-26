403e730d7a27c7d1716ca074cc23425f035e4858
ErikModica <erikmodica@gmail.com>
2021-03-26 12:05:20 -0700

    revisions

--- react-rendering-lists/src/index.jsx
+++ react-rendering-lists/src/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class RenderMon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.pokedex = props.pokedex;
-  }
 
   render() {
     const listItems = this.props.pokedex.map(pokemon => <li key={pokemon.number}>{pokemon.name}</li>);
