90e243ec5f40b3e870632b8f121a4abac2aaaa8a
ErikModica <erikmodica@gmail.com>
2021-03-28 15:22:13 -0700

    WIP

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,7 +4,7 @@ class Accordion extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { bodyVisible: false };
+    this.state = { currentID: null, currentTopicID: null };
     this.closeBody = this.closeBody.bind(this);
     this.decipherID = this.decipherID.bind(this);
   }
@@ -12,19 +12,22 @@ class Accordion extends React.Component {
   decipherID(event) {
     for (let i = 0; i < this.props.topics.length; i++) {
       if (this.props.topics[i].heading === event.target.textContent) {
-        this.setState({ currentID: this.props.topics[i].id });
+        this.setState({ currentID: this.props.topics[i].id, currentTopicID: this.props.topics[i].id });
+        console.log(this.state.currentID);
+        console.log(this.state.currentTopicID);
       }
     }
   }
 
   closeBody() {
-    this.setState({ currentID: 0 });
+    this.setState({ currentID: null });
+    console.log('yopy');
   }
 
   render() {
     const topicsList = this.props.topics.map(topic => (
       <div key={topic.id}>
-        <h1 onDoubleClick={this.closeBody} onClick={this.decipherID} className="head">{topic.heading}</h1>
+        <h1 onClick={this.state.currentTopicID === this.state.currentID ? this.decipherID : this.closeBody} className="head">{topic.heading}</h1>
         <p onClick={this.closeBody} className={topic.id === this.state.currentID ? 'body' : 'body hidden'}>{topic.body}</p>
       </div>
     ));
